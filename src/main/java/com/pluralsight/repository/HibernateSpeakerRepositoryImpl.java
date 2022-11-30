ae5b941df73f1b8ccef673f3348b815e34abcb8f
Nemanja Mitrovic <nemanja.mitrovic@en510092.endava.net>
2022-11-30 11:19:02 +0100

    Advanced Bean Configuration-FactoryBean Demo

--- src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
@@ -1,13 +1,18 @@
 package com.pluralsight.repository;
 
 import com.pluralsight.model.Speaker;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 @Repository("speakerRepository")
 public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
+
+    @Autowired
+    private Calendar cal;
 
     @Override
     public List<Speaker> findAll() {
@@ -17,6 +22,8 @@ public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
 
         speaker.setFirstName("Nemanjaaa");
         speaker.setLastName("Mitrovic");
+
+        System.out.println("cal: "+cal.getTime());
 
         speakers.add(speaker);
 
