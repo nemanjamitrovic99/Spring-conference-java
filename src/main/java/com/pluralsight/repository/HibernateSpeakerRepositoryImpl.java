e60b9696d7d216c18ffa1dc8f2c6a2c837ba42ea
Nemanja Mitrovic <nemanja.mitrovic@en510092.endava.net>
2022-11-30 11:36:36 +0100

    Advanced Bean Configuration-SpEL Demo

--- src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.pluralsight.repository;
 
 import com.pluralsight.model.Speaker;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -14,6 +15,9 @@ public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
     @Autowired
     private Calendar cal;
 
+    @Value("#{ T(java.lang.Math).random() * 100}")
+    private double seedNum;
+
     @Override
     public List<Speaker> findAll() {
         List<Speaker> speakers=new ArrayList<>();
@@ -22,6 +26,7 @@ public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
 
         speaker.setFirstName("Nemanjaaa");
         speaker.setLastName("Mitrovic");
+        speaker.setSeedNum(seedNum);
 
         System.out.println("cal: "+cal.getTime());
 
