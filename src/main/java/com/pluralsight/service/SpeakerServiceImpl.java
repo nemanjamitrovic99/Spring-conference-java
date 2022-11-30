7f66326aa8b5908d146c2b620c3491b85caca50b
Nemanja Mitrovic <nemanja.mitrovic@en510092.endava.net>
2022-11-30 10:34:03 +0100

    Advanced Bean Configuration-Bean Lifecycle Demo

--- src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -6,6 +6,7 @@ import com.pluralsight.repository.SpeakerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Service("speakerService")
@@ -20,6 +21,11 @@ public class SpeakerServiceImpl implements SpeakerService {
     public SpeakerServiceImpl (SpeakerRepository speakerRepository){
         System.out.println(("SpeakerServiceImpl repository constructor"));
         repository=speakerRepository;
+    }
+
+    @PostConstruct
+    private void initialize(){
+        System.out.println("We're called after the constructor");
     }
 
     @Override
